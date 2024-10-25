cc9c4282fa58c5d628ce274a8d9863aa50c9619a
Abdul Waqas <www.waqas7172@gmail.com>
2024-10-25 22:16:20 +0500

    add js assignment 17

---
+++ app.js
@@ -0,0 +1,29 @@
+function changeContent() {
+
+    let heading = document.getElementById("heading");
+    let para = document.getElementById("para");
+
+    heading.innerHTML = 'Java Script :';
+
+    para.innerHTML = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate debitis, culpa assumenda minima maxime aperiam suscipit obcaecati velit illo quod, porro aut ad dolore cumque, veniam praesentium molestias repudiandae facilis nostrum quam. Id autem, harum rerum nesciunt fugiat iure. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate debitis, culpa assumenda minima maxime aperiam suscipit obcaecati velit illo quod, porro aut ad dolore cumque, veniam praesentium molestias repudiandae facilis nostrum quam. Id autem, harum rerum nesciunt fugiat iure. <br><br>
+    
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quas sint hic mollitia quidem consectetur adipisci modi beatae cupiditate corrupti accusantium atque perferendis, iste soluta inventore nobis debitis. Fuga, minus numquam neque consequatur cupiditate nulla doloremque hic recusandae expedita, vero repudiandae vitae delectus cumque. Dolor recusandae nobis ullam laborum, ad repellat nisi magnam voluptate accusamus, repellendus debitis, quas reprehenderit in! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quas sint hic mollitia quidem consectetur adipisci modi beatae cupiditate corrupti accusantium atque perferendis, iste soluta inventore nobis debitis. Fuga, minus numquam neque consequatur cupiditate nulla doloremque hic recusandae expedita, vero repudiandae vitae delectus cumque. Dolor recusandae nobis ullam laborum, ad repellat nisi magnam voluptate accusamus, repellendus debitis, quas reprehenderit in! `
+
+    // alert('You have Change The Content By clicking the button')
+
+}
+
+function previousContent() {
+
+    let heading = document.getElementById("heading");
+    let para = document.getElementById("para");
+
+    heading.innerHTML = 'Casacading Style Sheet :';
+
+    para.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quas sint hic mollitia quidem consectetur adipisci modi beatae cupiditate corrupti accusantium atque perferendis, iste soluta inventore nobis debitis. Fuga, minus numquam neque consequatur cupiditate nulla doloremque hic recusandae expedita, vero repudiandae vitae delectus cumque. Dolor recusandae nobis ullam laborum, ad repellat nisi magnam voluptate accusamus, repellendus debitis, quas reprehenderit in! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quas sint hic mollitia quidem consectetur adipisci modi beatae cupiditate corrupti accusantium atque perferendis, iste soluta inventore nobis debitis. Fuga, minus numquam neque consequatur cupiditate nulla doloremque hic recusandae expedita, vero repudiandae vitae delectus cumque. Dolor recusandae nobis ullam laborum, ad repellat nisi magnam voluptate accusamus, repellendus debitis, quas reprehenderit in! <br><br>
+    
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate debitis, culpa assumenda minima maxime aperiam suscipit obcaecati velit illo quod, porro aut ad dolore cumque, veniam praesentium molestias repudiandae facilis nostrum quam. Id autem, harum rerum nesciunt fugiat iure. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate debitis, culpa assumenda minima maxime aperiam suscipit obcaecati velit illo quod, porro aut ad dolore cumque, veniam praesentium molestias repudiandae facilis nostrum quam. Id autem, harum rerum nesciunt fugiat iure. `
+
+    alert('Original Content')
+
+}
